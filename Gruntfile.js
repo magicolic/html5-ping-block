76b10e6648316c23b0de49188fe2791f8606899d
Philippe Ouellet <la_plume_en_or@hotmail.com>
2013-09-30 22:12:00 -0400

    Base structure and Grunt configuration

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        
+        jslint: { // https://npmjs.org/package/grunt-jslint / https://github.com/stephenmathieson/grunt-jslint
+            app: {
+                src: ['src/**/*.js']
+            },
+            test: {
+                src: ['test/**/*.js'],
+                directives: {
+                    predef: ['test', 'ok']
+                }
+            }
+        },
+        
+        jshint: { // https://npmjs.org/package/grunt-contrib-jshint
+            files: ['src/**/*.js', 'test/**/*.js']
+        },
+        
+        uglify: { // https://npmjs.org/package/grunt-contrib-uglify
+            my_target: {
+                files: {
+                    'build/app.min.js': ['src/**/*.js'],
+                    'build/test.min.js': ['test/**/*.js']
+                }
+            }
+        },
+        
+        qunit: { // https://npmjs.org/package/grunt-contrib-qunit
+            all: ['test/*.html']
+        }
+    });
+    
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-jslint');
+    
+    grunt.registerTask('default', ['jslint', 'jshint', 'uglify', 'qunit']);
+};
